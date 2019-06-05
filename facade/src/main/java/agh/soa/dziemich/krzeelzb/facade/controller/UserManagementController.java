e4cc4e8f3b8017b70d71a3e5ece02502d604cad1
Michał Dziedzic <michal.dziedzic@schibsted.com>
2019-06-05 11:48:43 +0200

    merge from sse-event-listener

--- facade/src/main/java/agh/soa/dziemich/krzeelzb/facade/controller/UserManagementController.java
+++ facade/src/main/java/agh/soa/dziemich/krzeelzb/facade/controller/UserManagementController.java
@@ -1,12 +1,19 @@
 package agh.soa.dziemich.krzeelzb.facade.controller;
 
-import agh.soa.dziemich.krzeelzb.facade.model.PasswordBody;
+import agh.soa.dziemich.krzeelzb.facade.listener.QueueListener;
 import agh.soa.dziemich.krzeelzb.services.IUserManagementDatabaseOperationsService;
 import javax.ejb.EJB;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.sse.OutboundSseEvent;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseEventSink;
 
 @Path("employee")
 public class UserManagementController {
@@ -14,13 +21,15 @@ public class UserManagementController {
   @EJB(lookup = "java:global/db/UserManagementDatabaseOperationService")
   IUserManagementDatabaseOperationsService userManagementDbOpService;
 
+
   //TODO people management
   @PUT
   @Path("/{id}/password/")
   public Response changePassword(@PathParam("id") Long id, String password){
     userManagementDbOpService.updatePassword(id, password);
-//    System.out.println("sratatata");
     return Response.ok().build();
   }
+
+
 
 }
---
+++ facade/src/main/java/agh/soa/dziemich/krzeelzb/facade/view/MessageHolder.java
@@ -0,0 +1,28 @@
+package agh.soa.dziemich.krzeelzb.facade.view;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class MessageHolder implements Serializable {
+
+
+  private List<String> responseMessagesList = new ArrayList<>();
+
+  public MessageHolder() {
+
+  }
+
+  public List<String> getResponseMessagesList() {
+    return responseMessagesList;
+  }
+
+  public void setResponseMessagesList(List<String> responseMessagesList) {
+    this.responseMessagesList = responseMessagesList;
+  }
+}
