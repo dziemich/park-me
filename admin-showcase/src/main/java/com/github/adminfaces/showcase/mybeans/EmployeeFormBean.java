f2973e2212be6eb8f977d6958e2c66b7226b3d24
Michał Dziedzic <michal.dziedzic@schibsted.com>
2019-06-08 00:07:57 +0200

    polished employee form

--- admin-showcase/src/main/java/com/github/adminfaces/showcase/mybeans/EmployeeFormBean.java
+++ admin-showcase/src/main/java/com/github/adminfaces/showcase/mybeans/EmployeeFormBean.java
@@ -14,6 +14,7 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.omnifaces.cdi.ViewScoped;
+import org.primefaces.json.JSONObject;
 
 
 @ViewScoped
@@ -23,7 +24,7 @@ public class EmployeeFormBean implements Serializable {
   String name;
   String login;
   String password;
-  Boolean isAdmin;
+  Boolean admin;
 
   public String getName() {
     return name;
@@ -50,11 +51,11 @@ public class EmployeeFormBean implements Serializable {
   }
 
   public Boolean getAdmin() {
-    return isAdmin;
+    return admin;
   }
 
   public void setAdmin(Boolean admin) {
-    isAdmin = admin;
+    this.admin = admin;
   }
 
   public List<Long> getIds() {
@@ -62,31 +63,19 @@ public class EmployeeFormBean implements Serializable {
   }
 
   public void addEmployee() {
-    Employee emp = new Employee(login, name, password, isAdmin);
-//    Client client = ClientBuilder.newClient();
-//    WebTarget target = client.target("http://127.0.0.1:8080/hr-management-service/hr/employees/post");
-//    Response response = target.request()
-//        .post(Entity.entity("\"{\"name\":\"admin\",\"login\":\"admin\",\"password\":\"admin\",\"isAdmin\":true}\"",
-//            MediaType.APPLICATION_JSON));
-//    int status = response.getStatus();
-//    ResteasyClient client = new ResteasyClientBuilder().build();
-//    Response response = client.target("http://localhost:8080/rest/users")
-//        .request("application/json").get();
-//    int status = response.getStatus();
-//    System.out.println("Status code: " + status);
+    Employee emp = new Employee(login, name, password, admin);
     System.out.println("jestem useless buttonem");
     ResteasyClient client = new ResteasyClientBuilder().build();
     ResteasyWebTarget target = client.target("http://127.0.0.1:8080/hr-management-service/hr/employees/post");
-    Response response = target.request()
-        .post(Entity.entity(""
-                + "{\n"
-                + "\t\"name\": \"admin\",\n"
-                + "\t\"login\": \"admin\",\n"
-                + "\t\"password\": \"admin\",\n"
-                + "\t\"isAdmin\": true\n"
-                + "}",
-            MediaType.APPLICATION_JSON));
+    JSONObject jsonObject = new JSONObject()
+        .put("name", emp.getName())
+        .put("login", emp.getLogin())
+        .put("password", emp.getPassword())
+        .put("isAdmin", emp.getAdmin());
 
+    String jsonStringified = jsonObject.toString();
+    Response response = target.request()
+        .post(Entity.entity(jsonStringified, MediaType.APPLICATION_JSON));
 
     int status = response.getStatus();
     System.out.println(status);
