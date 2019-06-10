a502c9ba6e08cde3b0fe16c817f44a1c02f8b809
Michał Dziedzic <michal.dziedzic@schibsted.com>
2019-06-10 20:38:35 +0200

    minor fixed to diplay, added working logout and moved sse to another webpage

--- admin-showcase/src/main/java/com/github/adminfaces/showcase/mybeans/UserBean.java
+++ admin-showcase/src/main/java/com/github/adminfaces/showcase/mybeans/UserBean.java
@@ -1,19 +1,50 @@
 package com.github.adminfaces.showcase.mybeans;
 
+import agh.soa.dziemich.krzeelzb.entities.Employee;
+import agh.soa.dziemich.krzeelzb.services.IUserManagementDatabaseOperationsService;
+import java.io.IOException;
 import java.io.Serializable;
 import java.security.Principal;
+import javax.ejb.EJB;
 import javax.ejb.Local;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.util.Faces;
 
 @Named
 @ViewScoped
 @Local
 public class UserBean implements Serializable {
 
+  @EJB(lookup = "java:global/db/UserManagementDatabaseOperationService")
+  IUserManagementDatabaseOperationsService userManagementDbOpService;
+
+
   public String findUser() {
     Principal principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
     return principal.getName();
+  }
+
+  public String showName(){
+    String login = findUser();
+    return userManagementDbOpService
+        .findAll()
+        .stream()
+        .filter(emp -> emp.getLogin().equals(login))
+        .map(Employee::getName)
+        .findFirst()
+        .orElseThrow(IllegalAccessError::new);
+  }
+
+  public void logout(){
+    Faces.getSession().invalidate();
+    ExternalContext ec = Faces.getExternalContext();
+    try {
+      ec.redirect("/showcase");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
