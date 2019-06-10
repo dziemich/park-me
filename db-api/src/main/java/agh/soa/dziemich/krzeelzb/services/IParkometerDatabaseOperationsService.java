8e94b81e275a0dd7f3cfceb647e62f4703bf05c0
krzeelzb <krzeczekelzbieta@gmail.com>
2019-06-10 15:32:40 +0200

    add subzone endpoint

---
+++ db-api/src/main/java/agh/soa/dziemich/krzeelzb/services/IParkometerDatabaseOperationsService.java
@@ -0,0 +1,7 @@
+package agh.soa.dziemich.krzeelzb.services;
+
+import agh.soa.dziemich.krzeelzb.entities.Parkometer;
+
+public interface IParkometerDatabaseOperationsService {
+    void addParkometer(Parkometer p);
+}
