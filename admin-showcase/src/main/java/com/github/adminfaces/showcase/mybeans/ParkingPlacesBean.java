6c2270eacde88d91f4ddd4e55a730bec6dbde875
krzeelzb <krzeczekelzbieta@gmail.com>
2019-06-10 18:07:15 +0200

    working adding parking places

--- admin-showcase/src/main/java/com/github/adminfaces/showcase/mybeans/ParkingPlacesBean.java
+++ admin-showcase/src/main/java/com/github/adminfaces/showcase/mybeans/ParkingPlacesBean.java
@@ -4,11 +4,18 @@ import agh.soa.dziemich.krzeelzb.entities.ParkingPlace;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import agh.soa.dziemich.krzeelzb.entities.Parkometer;
@@ -17,7 +24,10 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.omnifaces.cdi.ViewScoped;
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.json.JSONArray;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -31,15 +41,16 @@ public class ParkingPlacesBean implements Serializable {
   @EJB(lookup = "java:global/db/ParkingPlaceDatabaseOperationsService")
   IParkingPlaceDatabaseOperationsService parkingMeterDbOp;
 
+  private Long id;
+  private String street;
+  private Boolean taken;
+  private Boolean expired;
+
+  private LocalDateTime expirationTime;
+  private Date expirationTimeDate;
+
   public List<ParkingPlace> getParkingPlaces() throws IOException {
-//    return List.of(
-//        new ParkingPlace( "nullo", true, true, LocalDateTime.of(2019, 7,1,1,1,1)),
-//        new ParkingPlace( "rodzinna", false, true, LocalDateTime.of(2019, 7,1,1,1,1)),
-//        new ParkingPlace( "rodzinna", true, false, LocalDateTime.of(2019, 7,1,1,1,1) )
-//    );
-
    return parkingMeterDbOp.findAll();
-
   }
 
   public List<Long> getParkingPlacesId() throws IOException {
@@ -51,6 +62,25 @@ public class ParkingPlacesBean implements Serializable {
     return ids;
   }
 
+  public void onDateSelect(SelectEvent event) {
+    FacesContext facesContext = FacesContext.getCurrentInstance();
+    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+    facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+  }
+
+  public void click() {
+    PrimeFaces.current().ajax().update("form:display");
+    PrimeFaces.current().executeScript("PF('dlg').show()");
+  }
+
+  public LocalDateTime convertToLocalDateTime(Date dateToConvert) {
+    return LocalDateTime.ofInstant(
+            dateToConvert.toInstant(), ZoneId.systemDefault());
+  }
+  public void addParkingPlace(){
+    expirationTime=convertToLocalDateTime(expirationTimeDate);
+    parkingMeterDbOp.addParkingPlace(new ParkingPlace(street,taken,expired,expirationTime));
+    }
 
 
   public int numberOfFreePlaces() {
@@ -75,5 +105,61 @@ public class ParkingPlacesBean implements Serializable {
   }
   private JSONArray mapToJsonArray(String json){
     return new JSONArray(json);
+  }
+
+  public IParkingPlaceDatabaseOperationsService getParkingMeterDbOp() {
+    return parkingMeterDbOp;
+  }
+
+  public void setParkingMeterDbOp(IParkingPlaceDatabaseOperationsService parkingMeterDbOp) {
+    this.parkingMeterDbOp = parkingMeterDbOp;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public Boolean getTaken() {
+    return taken;
+  }
+
+  public void setTaken(Boolean taken) {
+    this.taken = taken;
+  }
+
+  public Boolean getExpired() {
+    return expired;
+  }
+
+  public void setExpired(Boolean expired) {
+    this.expired = expired;
+  }
+
+  public LocalDateTime getExpirationTime() {
+    return expirationTime;
+  }
+
+  public void setExpirationTime(LocalDateTime expirationTime) {
+    this.expirationTime = expirationTime;
+  }
+
+  public Date getExpirationTimeDate() {
+    return expirationTimeDate;
+  }
+
+  public void setExpirationTimeDate(Date expirationTimeDate) {
+    this.expirationTimeDate = expirationTimeDate;
   }
 }
